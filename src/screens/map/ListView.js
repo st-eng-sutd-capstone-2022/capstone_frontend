8411c5e326dac2f8bebef2fa1623bd5977d68c69
Mike Sun <mightnent@gmail.com>
2022-06-03 00:58:19 +0800

    listview datagrid

--- src/screens/map/ListView.js
+++ src/screens/map/ListView.js
@@ -6,11 +6,52 @@ const ListView = () => {
         <div style={{ height: 400, width: '100%' }}>
             <div style={{ display: 'flex', height: '100%' }}>
                 <div style={{ flexGrow: 1 }}>
-                    <DataGrid/>
+                    <DataGrid columns={columns} rows={rows}/>
                 </div>
             </div>
         </div>
     )
 }
+
+const columns = [
+    { field: 'boatId', headerName: 'ID' },
+    { field: 'status', headerName: 'Status'},
+    { field: 'battery',headerName: 'Battery'},
+    { field: 'weight',headerName: 'Last Load'},
+    { field: 'estimatedWeight',headerName: 'Estimated Weight'},
+    { field: 'zone',headerName: 'Zone'},
+  ];
+  
+  const rows = [
+    {
+        id: 1,
+        boatId: 'Ally',
+        status: 'active',
+        battery: '20',
+        weight: '20',
+        estimatedWeight: '21',
+        zone: '1',
+    },
+    {
+        id: 2,
+        boatId: 'Betty',
+        status: 'inactive',
+        battery: '80',
+        weight: '30',
+        estimatedWeight: '22',
+        zone: '2',
+    },
+    {
+        id: 3,
+        boatId: 'Cally',
+        status: 'moving',
+        battery: '80',
+        weight: '34',
+        estimatedWeight: '33',
+        zone: '3',
+    },
+      
+    
+  ];
 
 export default ListView;
