e25f1401187fc856452ede294d6a0bc3622de848
Mike Sun <mightnent@gmail.com>
2022-06-22 08:02:20 +0800

    list view

--- src/screens/map/ListView.js
+++ src/screens/map/ListView.js
@@ -1,57 +1,60 @@
 import React from "react";
 import { DataGrid } from '@mui/x-data-grid';
+import { useQuery } from "react-query";
+import axios from "axios";
+
+import {AuthContext} from '../../common/context/auth-context';
 
 const ListView = () => {
+
+    const auth = React.useContext(AuthContext);
+
+    const getLiveBoat = async() =>{
+        let {data} = await axios.get(
+            `${process.env.REACT_APP_BACKEND_URL}/location/liveboats`,{
+                headers: {
+                    'Authorization': `Bearer ${auth.token}`
+                  }
+            }
+        )
+        return data
+    }
+    const {isLoading:liveLoading,error:liveError, data:liveData} = useQuery('liveboats', getLiveBoat);
+    if(liveLoading){
+        return <h1>loading</h1>
+    }
+
+    const columns = [
+        { field: 'boatId', headerName: 'ID' },
+        { field: 'status', headerName: 'Status'},
+        { field: 'battery',headerName: 'Battery'},
+        { field: 'weight',headerName: 'Last Load'},
+        { field: 'estimatedWeight',headerName: 'Estimated Weight', width: 150},
+        { field: 'zone',headerName: 'Zone'},
+    ];
+
+    let rows = [];
+    liveData.map((boat,index)=>{
+        const row = {
+            id: index,
+            boatId: boat.boatID,
+            status: boat.status,
+            battery: boat.battery,
+            weight: boat.weight.kg,
+            estimatedWeight: boat.estimatedWeight,
+            zone: boat.zone,
+        }
+        rows.push(row)
+    })
+    
     return(
         <div style={{ height: 400, width: '95%',margin:'auto',marginTop:'20px'}}>
-            <div style={{ display: 'flex', height: '100%' }}>
-                <div style={{ flexGrow: 1 }}>
-                    <DataGrid columns={columns} rows={rows}/>
-                </div>
-            </div>
+          
+            <DataGrid columns={columns} rows={rows}/>
+               
         </div>
     )
 }
 
-const columns = [
-    { field: 'boatId', headerName: 'ID' },
-    { field: 'status', headerName: 'Status'},
-    { field: 'battery',headerName: 'Battery'},
-    { field: 'weight',headerName: 'Last Load'},
-    { field: 'estimatedWeight',headerName: 'Estimated Weight'},
-    { field: 'zone',headerName: 'Zone'},
-  ];
-  
-  const rows = [
-    {
-        id: 1,
-        boatId: 'Ally',
-        status: 'active',
-        battery: '20',
-        weight: '20',
-        estimatedWeight: '21',
-        zone: '1',
-    },
-    {
-        id: 2,
-        boatId: 'Betty',
-        status: 'inactive',
-        battery: '80',
-        weight: '30',
-        estimatedWeight: '22',
-        zone: '2',
-    },
-    {
-        id: 3,
-        boatId: 'Cally',
-        status: 'moving',
-        battery: '80',
-        weight: '34',
-        estimatedWeight: '33',
-        zone: '3',
-    },
-      
-    
-  ];
 
 export default ListView;
