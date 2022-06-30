094c48249d5fb8ea5ca4750f315d41bb43711bb3
Mike Sun <mightnent@gmail.com>
2022-06-30 14:31:25 +0700

    populate dg with get location

--- src/screens/assign/AssignLocation.js
+++ src/screens/assign/AssignLocation.js
@@ -1,9 +1,202 @@
-import React, { useState } from "react";
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { DataGrid, GridCellModes } from '@mui/x-data-grid';
+import { useNavigate } from 'react-router-dom';
+import { useQuery } from "react-query";
+import axios from "axios";
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import { useLocation } from 'react-router-dom';
 
-const AssignLocation = () => {
-    return(
-        <h1>location</h1>
-    );
+import {AuthContext} from '../../common/context/auth-context';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+function EditToolbar(props) {
+
+  const navigate = useNavigate();
+  
+  const handleMouseDown = (event) => {
+    // Keep the focus in the cell
+    event.preventDefault();
+    console.log("mousedown")
+    navigate('/assign/boat/add');
+  };
+
+  return (
+    <Box
+      sx={{
+        borderBottom: 1,
+        borderColor: 'divider',
+        p: 1,
+      }}
+    >
+      <Button
+        onMouseDown={handleMouseDown}
+        color="primary"
+        variant="outlined"
+        sx={{ mr: 1 }}
+        data-cy="add"
+        >
+        Add
+      </Button>
+    
+    </Box>
+  );
 }
 
-export default AssignLocation;
+export default function AssignLocation() {
+  const [open, setOpen] = React.useState(false);
+  const [msg,setMsg] = React.useState("");
+  const [severity,setSeverity] = React.useState("success");
+  const auth = React.useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  React.useEffect(()=>{
+    if(location?.state?.msg){
+        setOpen(true);
+        setMsg(location.state.msg)
+        navigate(location.pathname, {});  
+    }
+  },[location])
+
+  const getLocation = async () => {
+
+    let {data} = await axios.get(
+      `${process.env.REACT_APP_BACKEND_URL}/location`,{
+          headers: {
+            'Authorization': `Bearer ${auth.token}`
+          }
+      })
+    data.forEach((o,i)=>o.id=i+1);
+    data.forEach((o,i)=>o.zoneLength = o.zones.length)
+    return data;
+  }
+
+  const delAssign = async (rowBoatId) => {
+    axios.delete(
+        `${process.env.REACT_APP_BACKEND_URL}/assign/${rowBoatId}`,
+        {
+        headers: {
+            'Authorization': `Bearer ${auth.token}`
+        },
+            
+    }).then(res => {
+       setOpen(true);
+       setMsg("Successfully deleted boat");
+       navigate(location.pathname, {});  
+       window.location.reload();
+    })
+    .catch(function (error) {
+        console.log(error);
+        setOpen(true);
+        setSeverity("error");
+        setMsg("Error deleting boat")
+    });
+  }
+
+  const {isLoading, error, data, isFetching} = useQuery(`getlocation`, getLocation);
+  
+  if (isLoading) {
+    return (
+      <Box sx={{ display: 'flex',justifyContent: 'center',marginTop:'20%'}}>
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  const columns = [
+    { field:'_id',headerName:'ID',width:0,hide:true},
+    { field: 'location', headerName: 'Location', width: 120 },
+    { field: 'lat', headerName: 'Lat', width: 100},
+    { field: 'long', headerName: 'Lng', width: 100},
+    { field: 'zoneLength', headerName: 'Zones', width: 60},
+
+    // {
+    //   field: 'assignee',
+    //   headerName: 'Assigner',
+    //   width: 80,
+    // },
+    {
+      field: 'delete',
+      headerName: 'Delete',
+      disableClickEventBubbling: true,
+      renderCell: (params) => (
+        <strong>
+          {/* {params.id} */}
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            onClick={(event) => {
+              event.ignore = true;
+              delAssign(params.getValue(params.id,'location'));
+              event.stopPropagation();
+            }}
+            style={{ marginLeft: 16 }}
+          >
+            Delete
+          </Button>
+        </strong>
+      ),
+      width: 100,
+    },
+  
+  ];
+
+
+  
+  const rows = data
+ 
+  const handleOnClick= (rowParams)=>{
+    const rowBoatObjectId = rowParams.getValue(rowParams.id,'_id');
+    const rowBoatId = rowParams.getValue(rowParams.id,'boatId');
+    const rowSerial = rowParams.getValue(rowParams.id,'serialNumber');
+    const rowLocation = rowParams.getValue(rowParams.id,'location');
+    console.log(rowLocation);
+    navigate('/assign/boat/'+rowBoatObjectId,{
+      state: {
+        _id: rowBoatObjectId,
+        boatId: rowBoatId,
+        serialNumber: rowSerial,
+        location: rowLocation
+      }
+    });
+  }
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  return (
+    <div style={{ height: 500, width: '95%',margin:'auto',marginTop:'20px' }}>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }}>
+        {msg}
+        </Alert>
+      </Snackbar>
+      <DataGrid
+        sx={{cursor: 'pointer'}}
+        rows={rows}
+        columns={columns}
+        onRowClick={(params, event, details) => {
+          if (!event.ignore) {
+            handleOnClick(params);
+          }
+        }}
+        components={{
+          Toolbar: EditToolbar,
+        }}
+        
+      />
+    </div>
+  );
+}
